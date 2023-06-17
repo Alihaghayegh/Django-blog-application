bd10061708cf067739c28564626e8a24a037c33e
alihaghayegh <haghayeghali95@gmail.com>
2023-06-17 18:33:25 +0330

    pagination added and some bugs fixed

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -43,7 +43,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absulote_url(self):
+    def get_absolute_url(self):
         return reverse('blog:post_detail',
                         args=[self.publish.year,
                               self.publish.month,
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-# from django.http import Http404
 from .models import Post
+from django.core.paginator import Paginator
+
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.get_page(page_number)
+
     return render(
         request,
         'blog/post/list.html',
@@ -12,17 +17,14 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-    # try:
-    #     post = Post.published.get(id=id)
-    # except Post.DoesNotExist:
-    #     raise Http404("No Post found")
-
-    post = get_object_or_404(Post,
-                            status=Post.Status.PUBLISHED,
-                            slig=post,
-                            publish__year=year,
-                            publish__month=month,
-                            publish__day=day)
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
 
     return render(
         request,
