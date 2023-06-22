a5ba8bcc3a1ac69e5af37a5f02093d3e0a846b1f
alihaghayegh <haghayeghali95@gmail.com>
2023-06-22 10:11:43 +0330

    fixing bugs

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'haghayeghali95@gmail.com'
-EMAIL_HOST_PASSWORD = 'haghayegh1303'
+EMAIL_HOST_PASSWORD = '**********'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
