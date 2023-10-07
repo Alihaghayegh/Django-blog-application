af48188044022a3215bb80f55190de933012fb25
alihaghayegh <haghayeghali95@gmail.com>
2023-10-07 19:07:42 +0330

    formatting on blog/models.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -16,7 +16,8 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -43,17 +44,17 @@ class Post(models.Model):
                              self.publish.day,
                              self.slug])
 
+
 class Comment(models.Model):
     post = models.foreignKey(Post,
-                            on_delete=models.CASCADE,
-                            related_name='comments')
+                             on_delete=models.CASCADE,
+                             related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
     created = models.DateTimeField(auto_new_add=True)
     updated = models.DateTimeField(auto_row=True)
     active = models.BooleanField(default=True)
-
 
     class meta:
         ordering = ['created']
